ee8e684f9a3eca550cf0085aca69ec06b5d3dce5
Adam Spiers <blockchain@adamspiers.org>
2025-11-22 22:26:24 -0300

    fix polygon metrics

--- pkg/crawler/polygon.go
+++ pkg/crawler/polygon.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/prometheus/client_golang/prometheus"
 	cli "github.com/urfave/cli/v2"
 
 	"github.com/migalabs/armiarma/pkg/config"
@@ -199,8 +200,279 @@ func (c *PolygonCrawler) Close() {
 
 // GetMetrics returns the metrics module for the crawler
 func (c *PolygonCrawler) GetMetrics() *metrics.MetricsModule {
-	return metrics.NewMetricsModule(
-		"polygon_crawler",
+	metricsMod := metrics.NewMetricsModule(
+		"crawler",
 		"Polygon Crawler metrics",
 	)
+	// compose all the metrics
+	metricsMod.AddIndvMetric(c.clientDistributionMetrics())
+	metricsMod.AddIndvMetric(c.versionDistributionMetrics())
+	metricsMod.AddIndvMetric(c.geoDistributionMetrics())
+	metricsMod.AddIndvMetric(c.nodeDistributionMetrics())
+	metricsMod.AddIndvMetric(c.deprecatedNodeMetrics())
+	metricsMod.AddIndvMetric(c.getPeersOs())
+	metricsMod.AddIndvMetric(c.getPeersArch())
+	metricsMod.AddIndvMetric(c.getHostedPeers())
+	metricsMod.AddIndvMetric(c.getRTTDist())
+	metricsMod.AddIndvMetric(c.getIPDist())
+
+	return metricsMod
+}
+
+func (c *PolygonCrawler) clientDistributionMetrics() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(ClientDistribution)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		summary, err := c.DB.GetClientDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for cliName, cnt := range summary {
+			ClientDistribution.WithLabelValues(cliName).Set(float64(cnt.(int)))
+		}
+		return summary, nil
+	}
+	cliDist, err := metrics.NewIndvMetrics(
+		"client_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return cliDist
+}
+
+func (c *PolygonCrawler) versionDistributionMetrics() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(VersionDistribution)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		summary, err := c.DB.GetVersionDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for cliVer, cnt := range summary {
+			VersionDistribution.WithLabelValues(cliVer).Set(float64(cnt.(int)))
+		}
+		return summary, nil
+	}
+	versDist, err := metrics.NewIndvMetrics(
+		"client_version_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return versDist
+}
+
+func (c *PolygonCrawler) geoDistributionMetrics() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(GeoDistribution)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		summary, err := c.DB.GetGeoDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for country, cnt := range summary {
+			GeoDistribution.WithLabelValues(country).Set(float64(cnt.(int)))
+		}
+		return summary, nil
+	}
+	versDist, err := metrics.NewIndvMetrics(
+		"geographical_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return versDist
+}
+
+func (c *PolygonCrawler) nodeDistributionMetrics() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(NodeDistribution)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		peerLs, err := c.DB.GetNonDeprecatedPeers()
+		if err != nil {
+			return nil, err
+		}
+
+		NodeDistribution.Set(float64(len(peerLs)))
+
+		return len(peerLs), nil
+	}
+	nodeDist, err := metrics.NewIndvMetrics(
+		"geographical_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return nodeDist
+}
+
+func (c *PolygonCrawler) deprecatedNodeMetrics() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(DeprecatedCount)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		nodeCnt, err := c.DB.GetDeprecatedNodes()
+		if err != nil {
+			return nil, err
+		}
+		DeprecatedCount.Set(float64(nodeCnt))
+		return nodeCnt, nil
+	}
+	depNodes, err := metrics.NewIndvMetrics(
+		"deprecated_nodes",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return depNodes
+}
+
+func (c *PolygonCrawler) getPeersOs() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(OsDistribution)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		osDist, err := c.DB.GetOsDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for key, val := range osDist {
+			OsDistribution.WithLabelValues(key).Set(float64(val.(int)))
+		}
+		return osDist, nil
+	}
+	osMetr, err := metrics.NewIndvMetrics(
+		"os_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return osMetr
+}
+
+func (c *PolygonCrawler) getPeersArch() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(ArchDistribution)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		archDist, err := c.DB.GetArchDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for key, val := range archDist {
+			ArchDistribution.WithLabelValues(key).Set(float64(val.(int)))
+		}
+		return archDist, nil
+	}
+	archMetr, err := metrics.NewIndvMetrics(
+		"arch_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return archMetr
+}
+
+func (c *PolygonCrawler) getHostedPeers() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(HostedPeers)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		ipSummary, err := c.DB.GetHostingDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for key, val := range ipSummary {
+			HostedPeers.WithLabelValues(key).Set(float64(val.(int)))
+		}
+		return ipSummary, nil
+	}
+	ipHosting, err := metrics.NewIndvMetrics(
+		"hosted_peer_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return ipHosting
+}
+
+func (c *PolygonCrawler) getRTTDist() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(RttDist)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		summary, err := c.DB.GetRTTDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for key, val := range summary {
+			RttDist.WithLabelValues(key).Set(float64(val.(int)))
+		}
+		return summary, nil
+	}
+	indvMetric, err := metrics.NewIndvMetrics(
+		"rtt_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return indvMetric
+}
+
+func (c *PolygonCrawler) getIPDist() *metrics.IndvMetrics {
+	initFn := func() error {
+		prometheus.MustRegister(IPDist)
+		return nil
+	}
+	updateFn := func() (interface{}, error) {
+		summary, err := c.DB.GetIPDistribution()
+		if err != nil {
+			return nil, err
+		}
+		for key, val := range summary {
+			IPDist.WithLabelValues(key).Set(float64(val.(int)))
+		}
+		return summary, nil
+	}
+	indvMetric, err := metrics.NewIndvMetrics(
+		"ip_distribution",
+		initFn,
+		updateFn,
+	)
+	if err != nil {
+		return nil
+	}
+	return indvMetric
 }
